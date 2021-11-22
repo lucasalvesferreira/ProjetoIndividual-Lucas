bd536aa78f13a8513e2f66d5adc323dbb66a9729
unknown <lucas.aferreira@bandtec.com.br>
2021-11-22 19:46:18 -0300

    Ajustes para incerir no banco

--- Site_institucional/public/script/cadastro.js
+++ Site_institucional/public/script/cadastro.js
@@ -1,69 +1,69 @@
 
-    function limparFormulario() {
-        document.getElementById("form_cadastro").reset();
-    }
+function limparFormulario() {
+    document.getElementById("form_cadastro").reset();
+}
 
-    function cadastrar() {
-        aguardar();
+function cadastrar() {
+    aguardar();
 
-        var formulario = new URLSearchParams(new FormData(document.getElementById("form_cadastro")));
+    var formulario = new URLSearchParams(new FormData(document.getElementById("form_cadastro")));
 
-        var nome = formulario.get("nome");
-        var email = formulario.get("email");
-        var senha = formulario.get("senha");
-        var confirmacaoSenha = formulario.get("confirmacao-senha");
+    var nome = formulario.get("nome");
+    var email = formulario.get("email");
+    var senha = formulario.get("senha");
+    var confirmacaoSenha = formulario.get("confirmacaosenha");
 
-        // TODO: VERIFICAR AS VALIDAÇÕES QUE ELES ESTÃO APRENDENDO EM ALGORITMOS 
-        if (nome == "" || email == "" || senha == "" || confirmacaoSenha == "") {
-            
-            window.alert("Preencha todos os campos para prosseguir!");
-            if (nome == "")  {
-                console.log('nome está em branco')
-            }
-            if (email == "" )  {
-                console.log('email está em branco')
-            }
-            if (senha == "" )  {
-                console.log('senha está em branco')
-            }
-            if (confirmacaoSenha == "" )  {
-                console.log('confirmacaoSenha está em branco')
-            }
-            finalizarAguardar();
-            return false;
+    // TODO: VERIFICAR AS VALIDAÇÕES QUE ELES ESTÃO APRENDENDO EM ALGORITMOS 
+    if (nome == "" || email == "" || senha == "" || confirmacaoSenha == "") {
+
+        window.alert("Preencha todos os campos para prosseguir!");
+        if (nome == "") {
+            console.log('nome está em branco')
         }
-        
-        if (email.indexOf("@") == -1 || email.indexOf(".com") == -1) {
-            window.alert("Ops, e-mail inválido! Verifique e tente novamente.");
-            finalizarAguardar();
-            return false;
+        if (email == "") {
+            console.log('email está em branco')
         }
-        
-        if (senha != confirmacaoSenha) {
-            window.alert("As senhas inseridas devem ser iguais para prosseguir!");
-            finalizarAguardar();
-            return false;
+        if (senha == "") {
+            console.log('senha está em branco')
         }
-
-        fetch("/usuarios/cadastrar", {
-            method: "POST",
-            body: formulario
-        }).then(function (resposta) {
-
-            console.log("resposta: ", resposta);
-
-            if (resposta.ok) {
-                window.alert("Cadastro realizado com sucesso!");
-                window.location = "login.html";
-                limparFormulario();
-                finalizarAguardar();
-            } else {
-                throw("Houve um erro ao tentar realizar o cadastro!");
-            }
-        }).catch(function (resposta) {
-            console.log(`#ERRO: ${resposta}`);
-            finalizarAguardar();
-        });
-
+        if (confirmacaoSenha == "") {
+            console.log('confirmacaoSenha está em branco')
+        }
+        finalizarAguardar();
         return false;
     }
+
+    if (email.indexOf("@") == -1 || email.indexOf(".com") == -1) {
+        window.alert("Ops, e-mail inválido! Verifique e tente novamente.");
+        finalizarAguardar();
+        return false;
+    }
+
+    if (senha != confirmacaoSenha) {
+        window.alert("As senhas inseridas devem ser iguais para prosseguir!");
+        finalizarAguardar();
+        return false;
+    }
+
+    fetch("/usuarios/cadastrar", {
+        method: "POST",
+        body: formulario
+    }).then(function (resposta) {
+
+        console.log("resposta: ", resposta);
+
+        if (resposta.ok) {
+            window.alert("Cadastro realizado com sucesso!");
+            window.location = "login.html";
+            limparFormulario();
+            finalizarAguardar();
+        } else {
+            throw ("Houve um erro ao tentar realizar o cadastro!");
+        }
+    }).catch(function (resposta) {
+        console.log(`#ERRO: ${resposta}`);
+        finalizarAguardar();
+    });
+
+    return false;
+}
